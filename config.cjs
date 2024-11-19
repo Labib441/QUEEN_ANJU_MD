a10eb554529f50ed1f96bf71d6b177d7c755e59c
Labib441 <sabirhussainlabib381@gmail.com>
2024-11-19 22:27:33 +0600

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   "ALIVE_IMG": "https://raw.githubusercontent.com/Niko-AND-Janiya/ANJU-DATA/refs/heads/main/WhatsApp%20Image%202024-09-08%20at%209.00.17%20PM.jpeg",
-  "PREFIX": ".",
+  "PREFIX": "!",
   "AUTO_READ_STATUS": true,
   "MODE": "public",
   "BOT_NUMBER": "94717775628",
