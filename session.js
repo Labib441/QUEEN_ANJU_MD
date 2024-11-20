6fa47ff170351c2591985c30c364f8e26496332d
Labib441 <sabirhussainlabib381@gmail.com>
2024-11-20 08:05:20 +0600

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-MD=2B90VTKQ#hOC7F8i2sggbWNM6Y113zRBTVReNtdhfv-FH3gKMxFI",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=jYETyYKb#x4R_788vUoldGD47tzmwCczzdG9QhfKJL7WW-ieAGLo",
 };
